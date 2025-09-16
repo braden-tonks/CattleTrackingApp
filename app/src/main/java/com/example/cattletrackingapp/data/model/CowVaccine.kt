bf9689c9ef2b93db0a63d44585a448b3e3e60181
braden-tonks <braden.tonks@gmail.com>
2025-09-16 13:48:27 -0500

    Created Models for Bull and CowVaccine

--- app/src/main/java/com/example/cattletrackingapp/data/model/CowVaccine.kt
+++ app/src/main/java/com/example/cattletrackingapp/data/model/CowVaccine.kt
@@ -1,4 +1,15 @@
 package com.example.cattletrackingapp.data.model
 
-class CowVaccine {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CowVaccine (
+    val id: String,
+    val cow_id: String,
+    val calf_id: String,
+    val bull_id: String,
+    val vaccine_id: String,
+    val date_given: String,
+    val dose: String,
+    val remarks: String
+)
