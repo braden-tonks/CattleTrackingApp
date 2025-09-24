925e40c957fb862d30b207281bd67fb0e854e59d
Nick-Heislen <nlh85s@login.missouristate.edu>
2025-09-24 12:16:52 -0500

    Add Screens to the NavHost

--- app/src/main/java/com/example/cattletrackingapp/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/cattletrackingapp/ui/navigation/Screens.kt
@@ -4,4 +4,6 @@ sealed class Screen(val route: String) {
     object Home : Screen("home")
     object SearchByName : Screen("search_by_name")
     object SearchByRFID : Screen("search_by_rfid")
+    object AddCattle : Screen("add_cattle")
+    object CattleList : Screen("cattle_list")
 }
